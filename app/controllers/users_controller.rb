05d003b9198178daeccdf48ce3e6ebea5afee3e3
lekeDRS <leke.onileere@drs.network>
2018-09-17 09:33:34 +0800

    redirect to user pag on signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
 
     before_action :set_user, only: [:edit, :update, :show]
-    before_action :require_same_user, only: [edit, :update] 
+    before_action :require_same_user, only: [:edit, :update] 
     def new
         @user = User.new
 
@@ -10,8 +10,9 @@ class UsersController < ApplicationController
     def create
         @user = User.new(user_params)
         if @user.save
+            session[:user_id] = @user.id
             flash[:success] = "Welcome to the alpha blog #{@user.username}"
-            redirect_to articles_path
+            redirect_to user_path(@user)
         else 
          render 'new'   
         end
@@ -43,6 +44,7 @@ class UsersController < ApplicationController
     def require_same_user
         if !logged_in? && current_user != @user
             flash[danger] = "You can only edit your own account"
-            redirect_to root_path
+            redirect_to root_path 
+        end
     end
 end
